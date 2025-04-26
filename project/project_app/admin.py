0b366139251c6a62b4ffe99de117c997a82f8b25
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-04-26 17:55:03 +0530

    pages added

--- project/project_app/admin.py
+++ project/project_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+admin.site.register(Qualification)
+admin.site.register(Subject)
+admin.site.register(Course)
--- project/project_app/models.py
+++ project/project_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Qualification(models.Model):
+    name=models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+class Subject(models.Model):
+    qualification=models.ForeignKey(Qualification, on_delete=models.CASCADE)
+    name=models.CharField(max_length=50)
+
+class Course(models.Model):
+      INTERESTED_CHOICES = [
+        ('Job', 'Job'),
+        ('Study', 'Study'),
+       
+    ]
+      qualification=models.ForeignKey(Qualification, on_delete=models.CASCADE)
+      subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+      title = models.CharField(max_length=100)
+      description = models.TextField()
+      maindescription = models.TextField()
+      interested = models.CharField(max_length=20, choices=INTERESTED_CHOICES, null=True, blank=True)
+
